2e7874f9429c16adb035f06c3fbe6c39a616a91a
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-05-20 19:05:33 +0530

    Update update.py

--- update.py
+++ update.py
@@ -9,6 +9,7 @@ if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
         f.truncate(0)
 
+
 basicConfig(format='%(levelname)s | From %(name)s -> %(module)s line no: %(lineno)d | %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()], level=INFO)
 
